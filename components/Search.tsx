22ea8a662da86ff141487ed1d20e936cea28ae6a
Rozaliia Russkikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2024-01-04 09:44:05 -0800

    Style footer

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,9 +1,7 @@
 import image from "@/public/assets/rocks.png";
 
 const Search: React.FC = () => {
-  return (
-    <div className="bg-primary min-h-72 h-full text-background">Search</div>
-  );
+  return <div className="min-h-72 h-full text-background">Search</div>;
 };
 
 export default Search;
