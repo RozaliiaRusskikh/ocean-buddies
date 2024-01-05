3cb1141223c32adb5f7f34b1582bb6b63e2c05b3
Rozaliia Russkikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2024-01-04 17:10:12 -0800

    Add logic to login page

--- app/(site)/components/AuthLoginForm.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AuthLoginForm = () => {
-  return <div>AuthForm</div>;
-};
-
-export default AuthLoginForm;
--- app/(site)/login/page.tsx
+++ app/(site)/login/page.tsx
@@ -1,12 +1,12 @@
 import AuthPage from "../components/AuthPage";
-import AuthLoginForm from "../components/AuthLoginForm";
+import AuthForm from "../components/AuthForm";
 import { LayoutX } from "../components/LayoutX";
 
 const Login: React.FC = () => {
   return (
     <LayoutX className="flex min-h-screen flex-col justify-center items-center py-12 sm:px-6 lg:px-8">
       <AuthPage title="Sign in to your diver account" />
-      <AuthLoginForm />
+      <AuthForm />
     </LayoutX>
   );
 };
