95d0a7b3bba9247bae549c823f4080cf9f0ee9a8
Rozaliia Russkikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2024-01-05 16:46:02 -0800

    Rewrite bounce animation as per tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,21 +17,6 @@ const config: Config = {
         secondary: "#129DC0",
         background: "#FFFFFF",
       },
-      keyframes: {
-        bounce: {
-          "0%, 100%": {
-            transform: "translateY(-25%)",
-            animationTimingFunction: "cubic-bezier(0.8, 0, 1, 1)",
-          },
-          "50%": {
-            transform: "translateY(0)",
-            animationTimingFunction: "cubic-bezier(0, 0, 0.2, 1)",
-          },
-        },
-      },
-      animation: {
-        bounce: "bounce 1s infinite",
-      },
     },
   },
   plugins: [
