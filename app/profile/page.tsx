6c90f4d706a796dca4a221323fc57268eebc6504
Rozaliia Russkikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2024-01-22 14:13:49 -0800

    Securing profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,10 +1,50 @@
 "use client";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
 import { Text } from "../components/Text";
 import { signOut } from "next-auth/react";
+import spinner from "@/public/assets/spinner.svg";
+import { LayoutX } from "../components/LayoutX";
+import Link from "next/link";
 
 const Profile = () => {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return (
+      <LayoutX className="pt-10">
+        <div className="flex gap-3 justify-center">
+          <Image
+            className="animate-spin"
+            width={40}
+            height={40}
+            src={spinner}
+            alt="Spinner"
+          />
+          <Text as="h1" size="super" className="text-primary">
+            Processing...
+          </Text>
+        </div>
+      </LayoutX>
+    );
+  }
+
+  if (status === "unauthenticated") {
+    return (
+      <LayoutX className="pt-2">
+        <Text as="h1" size="super">
+          Access Denied.{" "}
+          <span className="block">
+            Please{" "}
+            <Link className="text-primary" href="login">
+              login
+            </Link>{" "}
+            to view your profile page.{" "}
+          </span>
+        </Text>
+      </LayoutX>
+    );
+  }
 
   const handleSignOut: React.MouseEventHandler<
     HTMLButtonElement
